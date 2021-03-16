6df69eefbfc645c820bc55c74cf915102dd64233
Fabio Aiello <fabio.aiello@appfolio.com>
2021-03-16 09:41:30 -0700

    basic react functionality

---
+++ app/components/Loading.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const styles = {
+    content: {
+        fontSize: '35px',
+        position: 'absolute',
+        left: '0',
+        right: '0',
+        marginTop: '20px',
+        textAlign: 'center'
+    }
+};
+
+export default class Loading extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: props.text
+        };
+    }
+
+    componentDidMount() {
+        const { speed, text } = this.props;
+        console.log('this.props: ', this.props);
+        console.log('text: ', text);
+
+        window.interval = window.setInterval(() => {
+            this.state.content === text + '...'
+                ? this.setState({ content: text })
+                : this.setState((prevState) => ({ content: prevState.content + '.' }))
+        }, speed)
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(window.interval);
+    }
+
+    render() {
+        return (
+            <p style={styles.content}>
+                {this.state.content}
+            </p>
+        )
+    }
+
+    
+}
+
+Loading.propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+}
+
+Loading.defaultProps = {
+    text: 'Loading',
+    speed: 300
+}
